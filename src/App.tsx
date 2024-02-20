95034954238c3e3b007988445a68fd3b8ba06a75
rora0320 <lkhpgrc@gmail.com>
2024-02-20 14:45:21 +0900

    한국 위치로 이동

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import {
     CameraFlyHome,
     CameraLookAt,
     PointPrimitiveCollection,
-    PointPrimitive
+    PointPrimitive, Camera
 } from "resium";
 import {Cartesian3, Color, Transforms} from "cesium";
 
@@ -18,17 +18,16 @@ const lookAtPoint = {
     offset:new Cartesian3(-74.0707383,40.7117244,10000000)
 }
 //카메라 기본위치 변경
-const flyToPoint=Cartesian3.fromDegrees(74.0707383,40.7117244,10000000);
+const flyToPoint=Cartesian3.fromDegrees(130.0707383,40.7117244,10000000);
 const center = Cartesian3.fromDegrees(75.59777, 40.03883);
 function App() {
     return (
         <Viewer full>
 
             {/*<CameraLookAt {...lookAtPoint}/>*/}
-
             <CameraFlyTo destination={flyToPoint}/>
-            <Entity position={Cartesian3.fromDegrees(74.0707383,40.7117244,100)} point={{ pixelSize: 10,color:Color.GREEN }} />
-            <Entity position={Cartesian3.fromDegrees(90.0707383,55.7117244,100)} point={{ pixelSize: 10 ,color:Color.YELLOW}} />
+            <Entity position={Cartesian3.fromDegrees(128.0707383,37.7117244,100)} point={{ pixelSize: 10,color:Color.BLUE }} />
+            <Entity position={Cartesian3.fromDegrees(127.0707383,35.7117244,100)} point={{ pixelSize: 10 ,color:Color.YELLOW}} />
             {/*<PointPrimitiveCollection modelMatrix={Transforms.eastNorthUpToFixedFrame(center)}>*/}
             {/*    <PointPrimitive color={Color.ORANGE} position={new Cartesian3(58.0,58.0,0.0)}/>*/}
             {/*    <PointPrimitive color={Color.YELLOW} position={new Cartesian3(1000000.0, 0.0, 0.0)}/>*/}
