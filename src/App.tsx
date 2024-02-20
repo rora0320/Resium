b5963e6794adf459ff1ee2aab97974aea1795cf1
rora0320 <lkhpgrc@gmail.com>
2024-02-20 14:36:27 +0900

    CameraFlyTo -> 카메라 시작위치변경 Entity->지도 위 포인터+ 클릭시 땅까지 PointPrimitiveCollection /PointPrimitive ->지도 위 포인터 CameraLookAt-> 지구 위 카메라->땅까지 줌하듯 감 point의 z축 해발고도

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,39 @@
-import {Viewer, Entity, PointGraphics, GeoJsonDataSource, KmlDataSource, Scene, Globe, Camera} from "resium";
-import { Cartesian3 } from "cesium";
+import {
+    Viewer,
+    Entity,
+    CameraFlyTo,
+    CameraFlyHome,
+    CameraLookAt,
+    PointPrimitiveCollection,
+    PointPrimitive
+} from "resium";
+import {Cartesian3, Color, Transforms} from "cesium";
+
 
 // 포인터 위치
 const position = Cartesian3.fromDegrees(74.0707383, 40.7117244, 100);
-const data = {
-    type: "Feature",
-    properties: {
-        name: "Coors Field",
-        amenity: "Baseball Stadium",
-        popupContent: "This is where the Rockies play!",
-    },
-    geometry: {
-        type: "Point",
-        coordinates: [-104.99404, 39.75621],
-    },
-};
-
+const pointGraphics = { pixelSize: 10 };
+const lookAtPoint = {
+    target:Cartesian3.fromDegrees(-74.0707383, 40.7117244, 100),
+    offset:new Cartesian3(-74.0707383,40.7117244,10000000)
+}
+//카메라 기본위치 변경
+const flyToPoint=Cartesian3.fromDegrees(74.0707383,40.7117244,10000000);
+const center = Cartesian3.fromDegrees(75.59777, 40.03883);
 function App() {
     return (
         <Viewer full>
-            <Scene>
-                <Globe>
-                    <Camera>
-                        <Entity position={position} name="Tokyo" description="Hello, world.">
-                            <PointGraphics pixelSize={10} />
-                        </Entity>
-                    </Camera>
-                </Globe>
-            </Scene>
-            {/*<GeoJsonDataSource data={"your_geo_json.geojson"} />*/}
-            {/*<KmlDataSource data={"your_geo_json.kml"} />*/}
-            {/*<GeoJsonDataSource data={data} />*/}
+
+            {/*<CameraLookAt {...lookAtPoint}/>*/}
+
+            <CameraFlyTo destination={flyToPoint}/>
+            <Entity position={Cartesian3.fromDegrees(74.0707383,40.7117244,100)} point={{ pixelSize: 10,color:Color.GREEN }} />
+            <Entity position={Cartesian3.fromDegrees(90.0707383,55.7117244,100)} point={{ pixelSize: 10 ,color:Color.YELLOW}} />
+            {/*<PointPrimitiveCollection modelMatrix={Transforms.eastNorthUpToFixedFrame(center)}>*/}
+            {/*    <PointPrimitive color={Color.ORANGE} position={new Cartesian3(58.0,58.0,0.0)}/>*/}
+            {/*    <PointPrimitive color={Color.YELLOW} position={new Cartesian3(1000000.0, 0.0, 0.0)}/>*/}
+            {/*</PointPrimitiveCollection>*/}
+
         </Viewer>
     );
 }
