13aac74036e2e18a39fb6119a2b8da0b7acf8de2
rora0320 <lkhpgrc@gmail.com>
2024-02-23 09:29:39 +0900

    b3dm ->Cesium3DTileset json 기본 예시

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import {
     CameraFlyHome,
     CameraLookAt,
     PointPrimitiveCollection,
-    PointPrimitive, Camera, CorridorGraphics, CylinderGraphics, CesiumComponentRef, Model,
+    PointPrimitive, Camera, CorridorGraphics, CylinderGraphics, CesiumComponentRef, Model, Cesium3DTileset,
 } from "resium";
 
 import * as Cesium from "cesium";
@@ -22,11 +22,11 @@ const lookAtPoint = {
     offset:new Cartesian3(-74.0707383,40.7117244,10000000)
 }
 //카메라 날아가서 위치 변경
-const flyToPoint=Cartesian3.fromDegrees(130.0707383,40.7117244,10000000);
+const flyToPoint=Cartesian3.fromDegrees(130.0707383,36.329436052073596,10000000);
 const center = Cartesian3.fromDegrees(125.59777, 40.03883);
 
 //model 위치 가져온것
-const origin = Cartesian3.fromDegrees(125.0, 40.0, 10);
+const origin = Cartesian3.fromDegrees(127.38196357284114, 36.329436052073596, 10);
 const rotationAngle = Cesium.Math.toRadians(-90);
 // y축 90도 회전된 프레임 생성
 const rotationMatrix = Matrix3.fromRotationY(rotationAngle); // Y축을 중심으로 90도 회전
@@ -72,6 +72,8 @@ function App() {
                 destination: flyPositionCartesian3,
                 duration: 2.0,
             });
+
+            setIsShow(true)
         } else {
             console.error('Unable to determine valid flyPosition');
         }
@@ -99,63 +101,68 @@ function App() {
     return (
         <Viewer full ref={viewerRef}>
 
-            {/*<CameraLookAt {...lookAtPoint}/>*/}
-            {/*<Camera />*/}
-            <CameraFlyTo destination={flyGetPosition}/>
-            {/*<Scene/>*/}
-            <Entity name='Corridor' position={Cartesian3.fromDegrees(128.0, 40.0,100)} onClick={getPosition}>
-                <CorridorGraphics
-                    material={Color.BLUE}
-                    positions={Cartesian3.fromDegreesArray([128.0, 40.0, 131.0, 40.0, 131.0, 35.0]) as any} // WORKAROUND
-                    height={200000.0}
-                    extrudedHeight={100000.0}
-                    width={200000.0}
-                    cornerType={CornerType.BEVELED}
-                    outline // height or extrudedHeight must be set for outlines to display
-                    outlineColor={Color.BLUE}
-                />
-            </Entity>
-            <Entity name='pointer' position={Cartesian3.fromDegrees(127.0707383,35.7117244,100)} point={{ pixelSize: 10 ,color:Color.YELLOW}} onClick={getPosition}>
-            </Entity>
-            <Entity name='Cylinder' position={Cartesian3.fromDegrees(127.0707383, 50.7117244, 100)} onClick={getPosition}>
-                <CylinderGraphics
-                    length={400000.0}
-                    topRadius={200000.0}
-                    bottomRadius={200000.0}
-                    material={Color.GREEN.withAlpha(0.5)}
-                    outline
-                    outlineColor={Color.DARKGREEN}
-                />
-            </Entity>
-            {/*<CameraFlyTo destination={cameraDest}/>*/}
-            {isShow&&
-                <>
-                <Model url={"Tile_+000_+002.obj_deoffset.glb"} modelMatrix={rotatedFixedFrame}
-                       minimumPixelSize={128}
-                       maximumScale={20000}
-                       onClick={getPosition}
-                >
-                </Model>
-                <Model url={"Tile_+000_+003.obj_deoffset.glb"} modelMatrix={rotatedFixedFrame}
-                       minimumPixelSize={128}
-                       maximumScale={20000}
-                       onClick={getPosition}
-                >
-                </Model>
-                <Model url={"Tile_+001_+001.obj_deoffset.glb"} modelMatrix={rotatedFixedFrame}
-                       minimumPixelSize={128}
-                       maximumScale={20000}
-                       onClick={getPosition}
-                >
-                </Model>
-                <Model url={"Tile_+001_+002.obj_deoffset.glb"} modelMatrix={rotatedFixedFrame}
-                               minimumPixelSize={128}
-                               maximumScale={20000}
-                               onClick={getPosition}
-                >
-                </Model>
-                </>
-            }
+            {/*/!*<CameraLookAt {...lookAtPoint}/>*!/*/}
+            {/*/!*<Camera />*!/*/}
+            {/*<CameraFlyTo destination={flyGetPosition}/>*/}
+            {/*/!*<Scene/>*!/*/}
+            {/*<Entity name='Corridor' position={Cartesian3.fromDegrees(128.0, 40.0,100)} onClick={getPosition}>*/}
+            {/*    <CorridorGraphics*/}
+            {/*        material={Color.BLUE}*/}
+            {/*        positions={Cartesian3.fromDegreesArray([128.0, 40.0, 131.0, 40.0, 131.0, 35.0]) as any} // WORKAROUND*/}
+            {/*        height={200000.0}*/}
+            {/*        extrudedHeight={100000.0}*/}
+            {/*        width={200000.0}*/}
+            {/*        cornerType={CornerType.BEVELED}*/}
+            {/*        outline // height or extrudedHeight must be set for outlines to display*/}
+            {/*        outlineColor={Color.BLUE}*/}
+            {/*    />*/}
+            {/*</Entity>*/}
+            {/*<Entity name='pointer' position={Cartesian3.fromDegrees(127.0707383,35.7117244,100)} point={{ pixelSize: 10 ,color:Color.YELLOW}} onClick={getPosition}>*/}
+            {/*</Entity>*/}
+            {/*<Entity name='Cylinder' position={Cartesian3.fromDegrees(127.0707383, 50.7117244, 100)} onClick={getPosition}>*/}
+            {/*    <CylinderGraphics*/}
+            {/*        length={400000.0}*/}
+            {/*        topRadius={200000.0}*/}
+            {/*        bottomRadius={200000.0}*/}
+            {/*        material={Color.GREEN.withAlpha(0.5)}*/}
+            {/*        outline*/}
+            {/*        outlineColor={Color.DARKGREEN}*/}
+            {/*    />*/}
+            {/*</Entity>*/}
+
+                {/*<Model url={"Tile_+000_+002.obj_deoffset.glb"} modelMatrix={rotatedFixedFrame}*/}
+                {/*       // minimumPixelSize={128}*/}
+                {/*       maximumScale={50}*/}
+                {/*       scale={5.0}*/}
+                {/*>*/}
+                {/*</Model>*/}
+                {/*<Model url={"Tile_+000_+003.obj_deoffset.glb"} modelMatrix={rotatedFixedFrame}*/}
+                {/*       // minimumPixelSize={128}*/}
+                {/*       maximumScale={50}*/}
+                {/*       scale={5.0}*/}
+                {/*>*/}
+                {/*</Model>*/}
+                {/*<Model url={"Tile_+001_+001.obj_deoffset.glb"} modelMatrix={rotatedFixedFrame}*/}
+                {/*       // minimumPixelSize={128}*/}
+                {/*       maximumScale={50}*/}
+                {/*       scale={5.0}*/}
+                {/*>*/}
+                {/*</Model>*/}
+                {/*<Model url={"Tile_+001_+002.obj_deoffset.glb"} modelMatrix={rotatedFixedFrame}*/}
+                {/*       // minimumPixelSize={128}*/}
+                {/*       maximumScale={50}*/}
+                {/*       scale={5.0}*/}
+                {/*>*/}
+                {/*</Model>*/}
+            <Cesium3DTileset
+                url={"test.json"}
+
+                onReady={tileset => {
+                    console.log('tileset',tileset)
+                  viewerRef.current?.cesiumElement?.zoomTo(tileset);
+                }}
+            />
+
             {/*<PointPrimitiveCollection modelMatrix={Transforms.eastNorthUpToFixedFrame(center)}>*/}
             {/*    <PointPrimitive color={Color.ORANGE} position={new Cartesian3(58.0,58.0,0.0)}  onClick={(e)=>getPosition(e)}/>*/}
             {/*    <PointPrimitive color={Color.YELLOW} position={new Cartesian3(100000.0, -200000.0, 0.0)}/>*/}
